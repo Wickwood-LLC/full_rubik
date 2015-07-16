6bdb9fbdd669f5733d7b0b7769060fda46e92b97
palagpatski <palagpatski@gmail.com>
2015-07-16 22:52:31 +0800

    Temporarily disabled script for the green bar.

--- full_rubik.js
+++ full_rubik.js
@@ -8,54 +8,28 @@
     }
   };
 
-  // Drupal.behaviors.secureSiteCookies = {
+  // Drupal.behaviors.secureSite = {
   //   attach: function (context, settings) {
-  //   	function setCookie(cname, cvalue, exdays) {
-		//     var d = new Date();
-		//     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-		//     var expires = "expires="+d.toUTCString();
-		//     document.cookie = cname + "=" + cvalue + "; " + expires;
-		// }
-
-		// function getCookie(cname) {
-		//     var name = cname + "=";
-		//     var ca = document.cookie.split(';');
-		//     for(var i=0; i<ca.length; i++) {
-		//         var c = ca[i];
-		//         while (c.charAt(0)==' ') c = c.substring(1);
-		//         if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-		//     }
-		//     return "";
-		// }
-
-		// function checkCookie() {
-		//     var user = getCookie("username");
-	 //        if (user != "" && user != null) {
-	 //            setCookie("username", user, 365);
-	 //        }
-		// }
-		// $(document).ready(checkCookie);
+  //   var dontShow = localStorage.getItem("dontShow");
+	 //  $('.close').click(function() {
+  //       $('#block-block-2').hide();
+  //       dontShow = true;
+  //       localStorage.setItem("dontShow", dontShow);
+  //       console.log(dontShow);
+  //     });
+  //     function checkShow() {
+  //     	dontShow = localStorage.getItem("dontShow");
+  //     	console.log(dontShow);
+  //     	if (dontShow = false) {
+  //     		$('#block-block-2').show();
+  //     	} else {
+  //     		$('#block-block-2').hide();
+  //     	}
+  //     }
+		// dontShow = false;
+		// localStorage.setItem("dontShow", dontShow);
+  //     $(document).ready(checkShow);
+  //     console.log(dontShow);
   //   }
   // };
-
-  Drupal.behaviors.secureSite = {
-    attach: function (context, settings) {
-    var dontShow = localStorage.getItem("dontShow");
-	  $('.close').click(function() {
-        $('#block-block-2').hide();
-        dontShow = true;
-        localStorage.setItem("dontShow", dontShow);
-        console.log(dontShow);
-      });
-      function checkShow() {
-      	dontShow = localStorage.getItem("dontShow");
-      	console.log(dontShow);
-      	if (dontShow = true) {
-      		$('#block-block-2').hide();
-      	}
-      }
-      $(document).ready(checkShow);
-      console.log(dontShow);
-    }
-  };
 }(jQuery));
