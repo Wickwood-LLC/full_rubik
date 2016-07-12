dc019524f46781027bfd35f1a1998241868e7648
palagpatski <palagpatski@gmail.com>
2016-07-12 16:08:55 +0800

    dpm form.

--- theme-settings.php
+++ theme-settings.php
@@ -12,7 +12,7 @@ function full_rubik_form_system_theme_settings_alter(&$form, $form_state, $form_
   if (isset($form_id)) {
     return;
   }
-  dpm($form_id);
+  dpm($form);
 
   $form['rubik'] = array(
     '#type' => 'fieldset',
