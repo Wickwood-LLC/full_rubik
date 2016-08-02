10e67aa741701b2644a11be7ddf1c800a1fb467f
palagpatski <palagpatski@gmail.com>
2016-08-02 15:38:02 +0800

    Move footer content into footer region container and remove feed icons

--- page.tpl.php
+++ page.tpl.php
@@ -59,11 +59,11 @@
 </div></div>
 
 <div id='footer' class='clearfix'>
-  <?php if ($feed_icons): ?>
+  <!-- <?php if ($feed_icons): ?>
     <div class='feed-icons clearfix'>
       <label><?php print t('Feeds') ?></label>
       <?php print $feed_icons ?>
     </div>
-  <?php endif; ?>
+  <?php endif; ?> -->
+  <?php print render($page['footer']); ?>
 </div>
-<?php print render($page['footer']); ?>
