95714f47323c337a0a97b52591727bc1191c27b3
palagpatski <palagpatski@gmail.com>
2017-09-26 15:39:25 +0800

    Trying to fix the issue with the floating menu not floating on bwma

--- full_rubik.js
+++ full_rubik.js
@@ -24,7 +24,7 @@
       $menu = $('#block-superfish-1');
       topSpacing = $('#admin-menu').height();
 
-      $(window).off("load resize");
+      $(window).off("load resize scroll");
 
       $(window).on("load resize", function() {
         menuWidth = $menu.parent().width();          // gets the width of the container
