ac85a22e5ed49a2fa2bd0dd5f94cc322283464a7
palagpatski <palagpatski@gmail.com>
2018-01-29 21:38:23 +0800

    Temporarily remove checks for the admin menu to see if the problem perists

--- full_rubik.js
+++ full_rubik.js
@@ -96,108 +96,108 @@
           });
         }
 
-        if ($('#admin-menu').length) {
-          windowTop = $(window).scrollTop() + topSpacing;    // tells how far our screen is currently from the top of the page
-          currentPosition = stickyTop - windowTop;    // tells how far our target element is from where our screen is currently
+        // if ($('#admin-menu').length) {
+        //   windowTop = $(window).scrollTop() + topSpacing;    // tells how far our screen is currently from the top of the page
+        //   currentPosition = stickyTop - windowTop;    // tells how far our target element is from where our screen is currently
 
-          if (currentPosition < 0) {   // if target element goes above the screen
-            $menu.css({
-              top: topSpacing,
-            });
-          }
-          else {
-            $menu.css({
-              top: '0',
-            });
-          }
-        }
+        //   if (currentPosition < 0) {   // if target element goes above the screen
+        //     $menu.css({
+        //       top: topSpacing,
+        //     });
+        //   }
+        //   else {
+        //     $menu.css({
+        //       top: '0',
+        //     });
+        //   }
+        // }
 
         // console.log("Top spacing is " + topSpacing);
       }
       
-      $('#admin-menu').on("mresize", function(){
-        menuWidth = $menu.parent().width();          // gets the width of the container
-        $menu.css({
-          width: menuWidth,
-        });
-        $menu.removeClass('sticky-menu');
+      // $('#admin-menu').on("mresize", function(){
+      //   menuWidth = $menu.parent().width();          // gets the width of the container
+      //   $menu.css({
+      //     width: menuWidth,
+      //   });
+      //   $menu.removeClass('sticky-menu');
 
-        $('#block-panels-mini-header').css({
-          "margin-bottom": 0
-        });
+      //   $('#block-panels-mini-header').css({
+      //     "margin-bottom": 0
+      //   });
 
-        stickyTop = $menu.offset().top;       // tells how far our target element is from the top of the page
-        windowTop = $(window).scrollTop();    // tells how far our screen is currently from the top of the page
-        currentPosition = stickyTop - windowTop;    // tells how far our target element is from where our screen is currently
-        menuHeight = $menu.height();        // gets the height of our menu
-        topSpacing = $('#admin-menu').height();
+      //   stickyTop = $menu.offset().top;       // tells how far our target element is from the top of the page
+      //   windowTop = $(window).scrollTop();    // tells how far our screen is currently from the top of the page
+      //   currentPosition = stickyTop - windowTop;    // tells how far our target element is from where our screen is currently
+      //   menuHeight = $menu.height();        // gets the height of our menu
+      //   topSpacing = $('#admin-menu').height();
 
-        // console.log('Distance from top of page: ' + stickyTop);
-        // console.log('Position on load ' + currentPosition);
+      //   // console.log('Distance from top of page: ' + stickyTop);
+      //   // console.log('Position on load ' + currentPosition);
 
-        if (currentPosition < 0) {   // if target element goes above the screen
-          $menu.css({
-            width: '100%',
-            left: '0',
-          });   //stick it at the top
-          $menu.addClass('sticky-menu');
+      //   if (currentPosition < 0) {   // if target element goes above the screen
+      //     $menu.css({
+      //       width: '100%',
+      //       left: '0',
+      //     });   //stick it at the top
+      //     $menu.addClass('sticky-menu');
           
-          if (!($("#block-superfish-1 #logo").length) && $(window).width() >= 720) {
-            $("#logo").clone().prependTo('#block-superfish-1');
-            $("#block-superfish-1 #logo").delay(500).queue(function() {
-              $("#block-superfish-1 #logo").addClass('animate').dequeue();
-            });
-          }
-          else if ($(window).width() < 720) {
-            $("#block-superfish-1 #logo").remove();
-          }
+      //     if (!($("#block-superfish-1 #logo").length) && $(window).width() >= 720) {
+      //       $("#logo").clone().prependTo('#block-superfish-1');
+      //       $("#block-superfish-1 #logo").delay(500).queue(function() {
+      //         $("#block-superfish-1 #logo").addClass('animate').dequeue();
+      //       });
+      //     }
+      //     else if ($(window).width() < 720) {
+      //       $("#block-superfish-1 #logo").remove();
+      //     }
 
-          if (!($("#block-superfish-1 .pane-page-site-name").length) && $(window).width() >= 870) {
-            $(".pane-page-site-name").clone().prependTo('#block-superfish-1');
-            $("#block-superfish-1 .pane-page-site-name").delay(500).queue(function() {
-              $("#block-superfish-1 .pane-page-site-name").addClass('animate').dequeue();
-            });
-          }
-          else if ($(window).width() < 870) {
-            $("#block-superfish-1 .pane-page-site-name").remove();
-          }
+      //     if (!($("#block-superfish-1 .pane-page-site-name").length) && $(window).width() >= 870) {
+      //       $(".pane-page-site-name").clone().prependTo('#block-superfish-1');
+      //       $("#block-superfish-1 .pane-page-site-name").delay(500).queue(function() {
+      //         $("#block-superfish-1 .pane-page-site-name").addClass('animate').dequeue();
+      //       });
+      //     }
+      //     else if ($(window).width() < 870) {
+      //       $("#block-superfish-1 .pane-page-site-name").remove();
+      //     }
 
-          $('#block-panels-mini-header').css({
-            "margin-bottom": menuHeight + 18
-          });
-        }
-        else {
-          $menu.css({
-            width: menuWidth,
-          });
-          $menu.removeClass('sticky-menu');
+      //     $('#block-panels-mini-header').css({
+      //       "margin-bottom": menuHeight + 18
+      //     });
+      //   }
+      //   else {
+      //     $menu.css({
+      //       width: menuWidth,
+      //     });
+      //     $menu.removeClass('sticky-menu');
 
-          $("#block-superfish-1 #logo").remove();
-          $("#block-superfish-1 .pane-page-site-name").remove();
+      //     $("#block-superfish-1 #logo").remove();
+      //     $("#block-superfish-1 .pane-page-site-name").remove();
 
-          $('#block-panels-mini-header').css({
-            "margin-bottom": 0
-          });
-        }
+      //     $('#block-panels-mini-header').css({
+      //       "margin-bottom": 0
+      //     });
+      //   }
 
-        if ($('#admin-menu').length) {
-          windowTop = $(window).scrollTop() + topSpacing;    // tells how far our screen is currently from the top of the page
-          currentPosition = stickyTop - windowTop;    // tells how far our target element is from where our screen is currently
+      //   // if ($('#admin-menu').length) {
+      //   //   windowTop = $(window).scrollTop() + topSpacing;    // tells how far our screen is currently from the top of the page
+      //   //   currentPosition = stickyTop - windowTop;    // tells how far our target element is from where our screen is currently
 
-          if (currentPosition < 0) {   // if target element goes above the screen
-            $menu.css({
-              top: topSpacing,
-            });
-          }
-          else {
-            $menu.css({
-              top: '0',
-            });
-          }
-        }
+      //   //   if (currentPosition < 0) {   // if target element goes above the screen
+      //   //     $menu.css({
+      //   //       top: topSpacing,
+      //   //     });
+      //   //   }
+      //   //   else {
+      //   //     $menu.css({
+      //   //       top: '0',
+      //   //     });
+      //   //   }
+      //   // }
 
-        // console.log("Top spacing is " + topSpacing);
-      });
+      //   // console.log("Top spacing is " + topSpacing);
+      // });
 
       $(window).scroll(function(){ // scroll event 
         $menu.addClass('sticky-menu');
@@ -207,36 +207,36 @@
         // console.log('Distance from top of page: ' + stickyTop);
         // console.log('Current position: ' + currentPosition);
 
-        if ($('#admin-menu').length) {
-          windowTop = $(window).scrollTop() + topSpacing;    // tells how far our screen is currently from the top of the page
-          currentPosition = stickyTop - windowTop;    // tells how far our target element is from where our screen is currently
-          console.log('[admin] Distance from top of page: ' + stickyTop);
-          console.log('[admin] Current position: ' + currentPosition);
+        // if ($('#admin-menu').length) {
+        //   windowTop = $(window).scrollTop() + topSpacing;    // tells how far our screen is currently from the top of the page
+        //   currentPosition = stickyTop - windowTop;    // tells how far our target element is from where our screen is currently
+        //   console.log('[admin] Distance from top of page: ' + stickyTop);
+        //   console.log('[admin] Current position: ' + currentPosition);
 
-          if (currentPosition < 0) {   // if target element goes above the screen
-            $menu.css({
-              top: topSpacing,
-              width: '100%',
-              left: '0',
-            });   //stick it at the top
-            $menu.addClass('sticky-menu');
+        //   if (currentPosition < 0) {   // if target element goes above the screen
+        //     $menu.css({
+        //       top: topSpacing,
+        //       width: '100%',
+        //       left: '0',
+        //     });   //stick it at the top
+        //     $menu.addClass('sticky-menu');
 
-            $('#block-panels-mini-header').css({
-              "margin-bottom": menuHeight + 18
-            });
-          }
-          else {
-            $menu.css({
-              top: '0',
-              width: menuWidth,
-            });
-            $menu.removeClass('sticky-menu');
+        //     $('#block-panels-mini-header').css({
+        //       "margin-bottom": menuHeight + 18
+        //     });
+        //   }
+        //   else {
+        //     $menu.css({
+        //       top: '0',
+        //       width: menuWidth,
+        //     });
+        //     $menu.removeClass('sticky-menu');
 
-            $('#block-panels-mini-header').css({
-              "margin-bottom": 0
-            });
-          }
-        }
+        //     $('#block-panels-mini-header').css({
+        //       "margin-bottom": 0
+        //     });
+        //   }
+        // }
 
         if (currentPosition < 0) {   // if target element goes above the screen
           $menu.css({
